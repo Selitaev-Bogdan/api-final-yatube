245a619bebbc10db74de46ee3b18c04d2f6f50a7
Богдан Селитаев <selitaevbogdan@gmail.com>
2026-04-21 22:26:59 +0800

    Fix API URLs routing

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,13 +6,20 @@ from .viewsets import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 router_v1 = DefaultRouter()
 router_v1.register('posts', PostViewSet, basename='posts')
 router_v1.register('groups', GroupViewSet, basename='groups')
-router_v1.register(
-    r'posts/(?P<post_id>\d+)/comments',
-    CommentViewSet,
-    basename='comments'
-)
 router_v1.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
-    path('v1/', include(router_v1.urls)),
+    path('', include(router_v1.urls)),
+    path(
+        'posts/<int:post_id>/comments/',
+        CommentViewSet.as_view({'get': 'list', 'post': 'create'}),
+        name='comments-list'
+    ),
+    path(
+        'posts/<int:post_id>/comments/<int:pk>/',
+        CommentViewSet.as_view(
+            {'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}
+        ),
+        name='comments-detail'
+    ),
 ]
