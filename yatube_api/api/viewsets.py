81ac1de5fff9b89b4be8363b8f683f8b94b99bf2
Богдан Селитаев <selitaevbogdan@gmail.com>
2026-04-22 17:18:09 +0800

    Disable pagination for all viewsets

--- yatube_api/api/viewsets.py
+++ yatube_api/api/viewsets.py
@@ -1,13 +1,11 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import filters, viewsets, mixins
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework import filters, mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from posts.models import Group, Post, Follow
+from posts.models import Follow, Group, Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (
-    CommentSerializer, GroupSerializer,
-    PostSerializer, FollowSerializer
+    CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer
 )
 
 
@@ -15,7 +13,7 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (IsAuthorOrReadOnly,)
-    pagination_class = LimitOffsetPagination
+    pagination_class = None
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -24,6 +22,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsAuthorOrReadOnly,)
+    pagination_class = None
 
     def get_queryset(self):
         post_id = self.kwargs.get('post_id')
@@ -40,6 +39,7 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = (IsAuthorOrReadOnly,)
+    pagination_class = None
 
 
 class FollowViewSet(
@@ -51,6 +51,7 @@ class FollowViewSet(
     permission_classes = (IsAuthenticated,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('following__username',)
+    pagination_class = None
 
     def get_queryset(self):
         return Follow.objects.filter(user=self.request.user)
